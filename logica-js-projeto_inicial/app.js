6040ae6efa20976cfba20ae3105f409a92964d42
Ethan Daniel <dansilveira383@gmail.com>
2024-06-19 11:53:38 -0300

    desafio 2 praticas com if-else

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10:');
 
 if(chute == numeroSecreto) {
-
+    alert(`Isso! Você descobriu o numero secreto ${numeroSecreto} :)`)
+} else {
+    alert('Você errou :(')
 }
