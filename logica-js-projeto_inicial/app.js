28b1e6c973e62b2d762f4e6f197c19156d27c357
Ethan Daniel <dansilveira383@gmail.com>
2024-06-19 13:13:02 -0300

    resolução mini-desafios com contador

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,19 @@
 alert('Boas vindos ao jogo do número secreto!');
 let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10:');
+let chute;
+let tentativas = 1;
 
-if(chute == numeroSecreto) {
-    alert(`Isso! Você descobriu o numero secreto ${numeroSecreto} :)`)
-} else {
-    alert('Você errou :(')
+while(chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10:');
+
+    if(chute == numeroSecreto) {
+        alert(`Isso! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas :)`);
+    } else {
+        if(numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
 }
