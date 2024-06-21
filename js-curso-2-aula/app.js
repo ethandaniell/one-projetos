540058b5dd4d426a01ead15d443cf4a1088857ec
Ethan Daniel <dansilveira383@gmail.com>
2024-06-21 11:55:39 -0300

    mini desafios realizados

--- js-curso-2-aula/app.js
+++ js-curso-2-aula/app.js
@@ -1,18 +1,56 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoTela('h1', 'Jodo do número secreto');
-exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoTela('h1', 'Jodo do número secreto');
+    exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    if(chute == numeroSecreto) {
+        exibirTextoTela('h1', 'Acertou!');
+        
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas} :)`; 
+        
+        exibirTextoTela('p', mensagemTentativas);
+        
+        document.querySelector('#reiniciar').removeAttribute('disabled');
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        
+        limparCampo();
+    }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = ' ';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+
+    exibirMensagemInicial();
+
+    document.querySelector('#reiniciar').setAttribute('disabled', true);
 }
